ed83ccba035c9460557d8442d41fc90c50c3f10f
Daniel Badinszky <badinszky97@gmail.com>
2024-09-29 15:02:23 +0200

    Hivatalos "word" példa működik

--- app/src/main/java/hu/nje/tankolasnyilvantarto/ElozoTankolasok.java
+++ app/src/main/java/hu/nje/tankolasnyilvantarto/ElozoTankolasok.java
@@ -3,19 +3,26 @@ package hu.nje.tankolasnyilvantarto;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
+
 public class ElozoTankolasok extends Fragment {
+    private WordViewModel mWordViewModel;
 
     public ElozoTankolasok() {
         // Required empty public constructor
     }
 
 
-    public static ElozoTankolasok newInstance(String param1, String param2) {
+    public static ElozoTankolasok newInstance() {
         ElozoTankolasok fragment = new ElozoTankolasok();
         Bundle args = new Bundle();
         fragment.setArguments(args);
@@ -26,7 +33,22 @@ public class ElozoTankolasok extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        MainActivity mainActivity = (MainActivity)getActivity();
+
         View view = inflater.inflate(R.layout.fragment_elozo_tankolasok, container, false);
+
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        final WordListAdapter adapter = new WordListAdapter(new WordListAdapter.WordDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(mainActivity.getBaseContext()));
+        mWordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
+
+        mWordViewModel.getAllWords().observe(getViewLifecycleOwner(), words -> {
+            // Update the cached copy of the words in the adapter.
+            adapter.submitList(words);
+        });
+
         return view;
     }
 }
